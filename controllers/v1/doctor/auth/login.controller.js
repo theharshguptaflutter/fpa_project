01e14578aa2e15c4943e6306593ce4c076acb686
harshsetstate <setstatedev.harsh@gmail.com>
2024-01-07 20:27:41 +0530

    yst

--- controllers/v1/doctor/auth/login.controller.js
+++ controllers/v1/doctor/auth/login.controller.js
@@ -33,7 +33,8 @@ async function login(req, res) {
   }
   //comparing the password of the registered user
   if (result == true || result === null) {
-    const otpcode = Math.floor(1000 + Math.random() * 9000);
+    //const otpcode = Math.floor(1000 + Math.random() * 9000);
+    const otpcode = "0000";
     if (doctor_email) {
       const transporter = nodemailer.createTransport({
         host: process.env.HOST_MAIL,
@@ -120,7 +121,8 @@ async function verifyemail(req, res) {
   });
 
   try {
-    const otpcode = Math.floor(1000 + Math.random() * 9000);
+   // const otpcode = Math.floor(1000 + Math.random() * 9000);
+    const otpcode = "0000";
 
     let verification_status = await userverify(email);
     const mailOptions = {

1c1f47f41512736c42915266405350128de24dde
harshsetstate <setstatedev.harsh@gmail.com>
2024-01-11 15:01:27 +0530

    newchange on login and doctor

--- controllers/v1/doctor/profile/profile.controller.js
+++ controllers/v1/doctor/profile/profile.controller.js
@@ -1,5 +1,5 @@
 const tableNames = require("../../../../utils/table_name");
-
+const bcrypt = require("bcrypt");
 const {
   success,
   error,
@@ -9,52 +9,51 @@ const editParameterQuery = require("../../../../utils/edit_query");
 const { s3Upload } = require("../../../../utils/s3_file_upload");
 
 async function userProfileUpdate(req, res) {
-  try {
-    var doctor_id = req.params.doctor_id;
-    var doctor_photo = req.body.photo;
-    var category_id = req.body.category_id;
+  //try {
+  var doctor_id = req.params.doctor_id;
+  var doctor_photo = req.body.photo;
+  var category_id = req.body.category_id;
 
-    if (doctor_photo != "") {
-      doctor_photo = await s3Upload(doctor_photo);
-    }
-
-    let profileUpdateInfo = {
-      state_id: req.body.state_id,
-      city_id: req.body.city_id,
-      doctor_name: req.body.doctor_name,
-      doctor_email: req.body.doctor_email,
-      doctor_number: req.body.doctor_number,
-      photo: req.body.photo,
-      password:  bcrypt.hashSync(String(req.body.password), 10),
-      doctor_occupation: req.body.doctor_occupation,
-      doctor_specialist: req.body.doctor_specialist,
-      doctor_profile_update: 1,
-    };
-    var userProfileUpdateParamiter = await editParameterQuery(
-      profileUpdateInfo
-    );
-    const userProfileupdateQuery = tableNames.doctorUser.update(
-      userProfileUpdateParamiter,
-      {
-        where: {
-          doctor_id: doctor_id,
-        },
-      }
-    );
-    if (userProfileupdateQuery != null) {
-      let doctorCategoryInfo = {
-        doctor_id: doctor_id,
-        category_id: category_id,
-      };
-      const doctorCategoryInsertQuery = tableNames.doctorCategory.create(doctorCategoryInfo);
-      success(res, "Profile has been updated", 200, 1);
-    } else {
-      res.statusCode= 404;
-      error(res, "Profile  not updated please try again later ");
-    }
-  } catch (err) {
-    error(res, err, 500);
+  if (doctor_photo != "") {
+    doctor_photo = await s3Upload(doctor_photo);
   }
+
+  let profileUpdateInfo = {
+    state_id: req.body.state_id,
+    city_id: req.body.city_id,
+    doctor_name: req.body.doctor_name,
+    doctor_email: req.body.doctor_email,
+    doctor_number: req.body.doctor_number,
+    photo: req.body.photo,
+    password: bcrypt.hashSync(String(req.body.password), 10),
+    doctor_occupation: req.body.doctor_occupation,
+    doctor_specialist: req.body.doctor_specialist,
+    doctor_profile_update: 1,
+  };
+  var userProfileUpdateParamiter = await editParameterQuery(profileUpdateInfo);
+  const userProfileupdateQuery = tableNames.doctorUser.update(
+    userProfileUpdateParamiter,
+    {
+      where: {
+        doctor_id: doctor_id,
+      },
+    }
+  );
+  if (userProfileupdateQuery != null) {
+    let doctorCategoryInfo = {
+      doctor_id: doctor_id,
+      category_id: category_id,
+    };
+    const doctorCategoryInsertQuery =
+      tableNames.doctorCategory.create(doctorCategoryInfo);
+    success(res, "Profile has been updated", 200, 1);
+  } else {
+    res.statusCode = 404;
+    error(res, "Profile  not updated please try again later ");
+  }
+  // } catch (err) {
+  //   error(res, err, 500);
+  // }
 }
 
 async function getUserProfile(req, res) {

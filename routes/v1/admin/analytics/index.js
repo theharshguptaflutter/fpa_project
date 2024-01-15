e6709037570570de32086ed2fb3186bc4ce6c757
harshsetstate <setstatedev.harsh@gmail.com>
2024-01-16 01:07:36 +0530

    new  changes

--- routes/v1/admin/analytics/index.js
+++ routes/v1/admin/analytics/index.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const router = express.Router();
-const {} = require("../../../../controllers/v1/admin/analytics/analytics.controller");
+const {} = require("../../../../controllers/v1/admin/analytics/doctor_analytics/doctor_analytics.controller");
+const {} = require("../../../../controllers/v1/admin/analytics/user_analytics/user_analytics.controller.js");
 
+
+
+//doctor_analytics start
+router.get("/doctor_analytics", (req, res) => {
+  console.log("okk");
+});
+//doctor_analytics end
+
+
+
+
+
+
+
+///user_analytics start
 router.get("/user_analytics", (req, res) => {
   console.log("okk");
 });
 
+///user_analytics end
 module.exports = router;

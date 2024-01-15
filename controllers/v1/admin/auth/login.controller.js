5d00074fa7951c35749a2415760328dbce99529c
Murtaza <murtazabohraa@gmail.com>
2024-01-16 00:50:06 +0530

    login and profile controller for admin js added

--- controllers/v1/admin/auth/login.controller.js
+++ controllers/v1/admin/auth/login.controller.js
@@ -1,2 +1,159 @@
-//login for superadmin and also allows it to create and validate other admins
+const tableNames = require("../../../../utils/table_name");
+const bcrypt = require("bcrypt");
+const editParameterQuery = require("../../../../utils/edit_query");
+const {
+  success,
+  error,
+  successWithdata,
+  success1,
+  resetpasswordsucess,
+  resetpassworderror,
+  verifyemailsucess,
+} = require("../../../../utils/responseApi");
 
+var jwt = require("jsonwebtoken");
+
+async function login(req, res) {
+  const email = req.body.email;
+  const pwd = req.body.password;
+  const client_id = req.body.clientId;
+
+  if (!email || !pwd) {
+    res.statusCode = 404;
+    return error(res, "Please provide both email and password");
+  }
+
+  if (email && pwd) {
+    let user = await tableNames.User.findOne({
+      where: { email: email },
+    });
+    if (!user || !user.password) {
+      res.statusCode = 404;
+      return error(res, "User not found or password not set.");
+    }
+    const passChk = await bcrypt.compare(String(pwd), user.password);
+    if (!passChk) {
+      res.statusCode = 404;
+      return error(res, "Incorrect Password!");
+    }
+    //Generating JWT token and sending user data
+    const privatekey = process.env.privateKey;
+    let params = {
+      user_id: user["user_id"],
+      user_number: user["user_number"],
+    };
+    const token = await jwt.sign(params, privatekey, {
+      expiresIn: "365d",
+    });
+
+    if (!token) {
+      res.statusCode = 409;
+      error(res, "Token not generated");
+    } else {
+      let tokeninfo = {
+        user_id: user["user_id"],
+        user_number: user["user_number"],
+        gen_token: token,
+      };
+      const accessTokensGenInsetQuery = await tableNames.accessTokens.create(
+        tokeninfo
+      );
+      if (!accessTokensGenInsetQuery) {
+        res.statusCode = 409;
+        error(res, "Generated token not inserted into db");
+      } else {
+        if (!client_id) {
+          res.statusCode = 404;
+          return error(res, "Unable to access the Client Id!");
+        } else {
+          let checkClientId = await tableNames.clientAccessToken.findOne({
+            where: { user_id: user["user_id"], client_id: client_id },
+          });
+          if (checkClientId === null) {
+            const cliendIdInsertQuery =
+              await tableNames.clientAccessToken.create({
+                user_id: user["user_id"],
+                client_id: client_id,
+              });
+            if (!cliendIdInsertQuery) {
+              res.statusCode = 409;
+              error(res, "Client Id not inserted into DB!");
+            }
+          }
+          return res.status(200).send({
+            status: 200,
+            isuserfound: true,
+            message: "Login successful",
+            user_details: [
+              {
+                user_id: user["user_id"],
+                user_role: user["role_id"] ?? " ",
+                name: user["name"] ?? " ",
+                email: user["email"] ?? " ",
+                user_number: user["user_number"] ?? " ",
+                city_id: user["city_id"] ?? " ",
+                state_id: user["state_id"] ?? " ",
+                user_online_status: user["user_online_status"],
+                user_delete_flag: user["user_delete_flag"],
+                client_id: client_id ?? " ",
+                token: token ?? " ",
+                avatar: user["avatar"] ?? " ",
+              },
+            ],
+          });
+        }
+      }
+    }
+  }
+}
+
+async function passwordrecovery(req, res) {
+  const user_id = req.body.user_id;
+  let profileUpdateInfo = {
+    password: bcrypt.hashSync(String(req.body.password), 10),
+  };
+  var userProfileUpdateParamiter = await editParameterQuery(profileUpdateInfo);
+  const userProfileupdateQuery = tableNames.User.update(
+    userProfileUpdateParamiter,
+    {
+      where: {
+        user_id: user_id,
+      },
+    }
+  );
+  if (userProfileupdateQuery != null) {
+    verifyemailsucess(res, "Password has been changed");
+  } else {
+    res.statusCode = 304;
+    error(res, "Profile  not updated please try again later ");
+  }
+}
+
+async function logout(req, res) {
+  try {
+    var user_id = req.params.user_id;
+
+    const updateQuery = tableNames.User.update(
+      { user_online_status: 1 },
+      {
+        where: {
+          user_id: user_id,
+        },
+      }
+    );
+    if (updateQuery != null) {
+      success1(res, "user has been logout", 200);
+    } else {
+      res.statusCode = 209;
+      error(res, "unable to logout please try again later ");
+    }
+  } catch (error) {
+    error(res, error, 500);
+  }
+}
+
+module.exports = {
+  login,
+  logout,
+  passwordrecovery,
+};
--- controllers/v1/admin/profile/profile.controller.js
+++ controllers/v1/admin/profile/profile.controller.js
@@ -1,1 +1,78 @@
-// update profile
+// update admin and staff profiles
+const tableNames = require("../../../../utils/table_name");
+
+const {
+  success,
+  error,
+  successWithdata,
+} = require("../../../../utils/responseApi");
+const editParameterQuery = require("../../../../utils/edit_query");
+const bcrypt = require("bcrypt");
+
+async function userProfileUpdate(req, res) {
+
+
+  try {
+    var user_id = req.params.user_id;
+    var password = req.body.password;
+    if (password){
+      password = bcrypt.hashSync(String(password), 10);
+    }
+    let profileUpdateInfo = {
+      email: req.body.email,
+      password: password,
+    };
+    var userProfileUpdateParamiter = await editParameterQuery(
+      profileUpdateInfo
+    );
+    const userProfileupdateQuery = await tableNames.User.update(
+      userProfileUpdateParamiter,
+      {
+        where: {
+          user_id: user_id,
+        },
+      }
+    );
+   
+    if (userProfileupdateQuery != 0) {
+      const updatedUserData = await tableNames.User.findOne({
+        where: { user_id: user_id },
+      });
+
+      console.log(updatedUserData);
+
+      successWithdata(
+        res,
+        "Profile has been updated",
+        200,
+        updatedUserData.toJSON()
+      );
+    } else {
+      res.statusCode = 404;
+      error(res, "Profile  not updated please try again later ");
+    }
+  } catch (err) {
+    error(res, err, 500);
+  }
+}
+
+async function getUserProfile(req, res) {
+  var user_id = req.params.user_id;
+  let userFindQuery = await tableNames.User.findOne({
+    where: { user_id: user_id, user_delete_flag: 0 },
+  });
+  if (userFindQuery != null || userFindQuery != "") {
+    successWithdata(
+      res,
+      "User profle details found",
+      "User profile details not found",
+      userFindQuery,
+      0
+    );
+  }
+}
+
+module.exports = {
+  userProfileUpdate,
+  getUserProfile,
+};
